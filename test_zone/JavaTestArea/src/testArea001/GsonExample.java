88dff8a0331481c82ff43a08c1a7914e9fd94efa
Yaron Kessler <Yaron.Kessler@brinks.com>
2017-11-19 17:11:25 +0200

    test area

---
+++ test_zone/JavaTestArea/src/testArea001/GsonExample.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package testArea001;
+
+/**
+ * @author kesslery
+ *
+ */
+public class GsonExample {
+
+	/**
+	 * 
+	 */
+	public GsonExample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
