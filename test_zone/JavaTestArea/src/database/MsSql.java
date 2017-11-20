c2ccfb833624a2fbccf3ea3a97310dab45cd9140
Yaron Kessler <Yaron.Kessler@brinks.com>
2017-11-20 15:29:51 +0200

    connection

---
+++ test_zone/JavaTestArea/src/database/MsSql.java
@@ -0,0 +1,14 @@
+package database;
+
+public class MsSql {
+
+	public MsSql() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
