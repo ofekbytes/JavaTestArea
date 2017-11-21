8c8f997399fdac816adb826ce05f2e1b2d8e6c14
Yaron Kessler <Yaron.Kessler@brinks.com>
2017-11-21 18:30:27 +0200

    test

---
+++ test_zone/JavaTestArea/src/sql/MsSql.java
@@ -0,0 +1,35 @@
+//package database;
+//
+//import java.sql.*;
+//
+//public class MsSql 
+//{
+//
+//	//static final void String JDBC_DRIVER = "";
+//	
+//	
+//	/****
+//	 * 
+//	 * @return true= connection succeeded (OK)
+//	 * @return false= connection failed (NOT-OK)
+//	 */
+//	public boolean connect () 
+//	{
+//		
+//		
+//		
+//		
+//		return false; 
+//	}
+//	
+//	
+//	public MsSql() {
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}
